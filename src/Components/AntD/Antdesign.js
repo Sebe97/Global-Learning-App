a36253f0d87dd78617ab9ae006c475d26271b086
SebastianAndreiBaciu <sebe97@yahoo.com>
2024-03-25 19:12:42 +0200

    Fix antd

--- src/Components/AntD/Antdesign.js
+++ src/Components/AntD/Antdesign.js
@@ -1,34 +1,55 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Modal } from 'antd';
 import '../../App.css'; // CSS file for custom styling
+
+// The goal is to have a resizable modale on the vertical axis
 
 const Antdesign = () => {
   const [height, setHeight] = useState(400); // Initial height of the modal
   const initialY = useRef(400); // Initial Y coordinate of mouse when resizing starts
+
+  const [open, setOpen] = useState(false);
 
   const handleResize = (e) => {
     const diff = e.clientY - initialY.current;
     setHeight(height + diff);
   };
 
+  const openModal = () => {
+    setOpen(true);
+  };
+
+  const handleOnCancel = () => {
+    setOpen(false);
+  };
+
+  const handleOnOk = () => {
+    setOpen(false);
+  };
+
   return (
-    <Modal
-      open={true}
-      bodyStyle={{ height: `${height}px` }} // Set the height dynamically
-      wrapClassName="resizable-modal"
-    >
-      <div
-        className="resize-handle"
-        onMouseDown={(e) => {
-          initialY.current = e.clientY;
-          document.addEventListener('mousemove', handleResize);
-          document.addEventListener('mouseup', () => {
-            document.removeEventListener('mousemove', handleResize);
-          });
-        }}
-      />
-      Modal content
-    </Modal>
+    <>
+      <button onClick={openModal}> Open Modal</button>
+      <Modal
+        open={open}
+        onOk={handleOnOk}
+        onCancel={handleOnCancel}
+        bodyStyle={{ height: `${height}px` }} // Set the height dynamically
+        wrapClassName="resizable-modal"
+      >
+        <div
+          className="resize-handle"
+          onMouseDown={(e) => {
+            initialY.current = e.clientY;
+            document.addEventListener('mousemove', handleResize);
+            document.addEventListener('mouseup', () => {
+              document.removeEventListener('mousemove', handleResize);
+            });
+          }}
+        />
+        Modal content
+      </Modal>
+    </>
   );
 };
 
